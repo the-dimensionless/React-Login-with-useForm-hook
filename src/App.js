f609ab6f318c7ec2eb8b74b42d367d91d5c24ce7
Sumit Singh <sumit.sssingh.singh@gmail.com>
2020-08-21 21:08:35 +0530

    added react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
+
+import useForm from 'react-hoo-form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="email" placeholder="Enter email" name="email" />
+        <input type="password" placeholder="Enter password" name="password" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
